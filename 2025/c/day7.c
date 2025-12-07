5ec4a419829a1f3e02b579a0c9660265fcf41589
Igor Karablin <igorkarablin@firstbeatmedia.com>
2025-12-07 23:31:13 +0400

    Day 7, part 2

--- 2025/c/day7.c
+++ 2025/c/day7.c
@@ -4,19 +4,31 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <ctype.h>
 
 #include "common.h"
 
-DARRAY_DEFINE_TYPE(String, char);
-DARRAY_DEFINE_TYPE(StringList, String);
+enum {
+    TREE_START = -1,
+    TREE_SPLIT = -2,
+};
 
-bool read_line(String *s, FILE *f)
+DARRAY_DEFINE_TYPE(IntArray, int64_t);
+
+// read and parse into int array (allows to count combinations inplace)
+bool read_line(IntArray *a, FILE *f)
 {
-    s->length = 0;
+    a->length = 0;
     for (int c = fgetc(f); c != '\n' && c != EOF; c = fgetc(f)) {
-        DARRAY_PUSH(*s, c);
+        int64_t v = 0;
+        if (c == 'S') {
+            v = TREE_START;
+        } else if (c == '^') {
+            v = TREE_SPLIT;
+        }
+        DARRAY_PUSH(*a, v);
     }
-    return s->length;
+    return a->length;
 }
 
 int main(int argv, char* argc[])
@@ -32,42 +44,53 @@ int main(int argv, char* argc[])
         return -1;
     }
 
-    String first_line = {0};
-    String second_line = {0};
-    int answer1 = 0;
+    IntArray first_line = {0}, second_line = {0};
+    uint64_t answer1 = 0, answer2 = 0;
+
+    /*
+        propagate combinations down. '^' adds number above to the left and right, 
+        sum of values in last line is the answer for part 2. answer 1 is '^' count.
+
+        .......S.......
+        .......1.......
+        ......1^1......
+        ......1.1......
+        .....1^2^1.....
+        .....1.2.1.....
+        ....1^3^3^1....
+        ....1.3.3.1....
+        ...1^4^331^1...
+        ...1.4.331.1...
+    */
 
     read_line(&first_line, f);
     while(read_line(&second_line, f)) {
-        fwrite(first_line.data, 1, first_line.length, stdout);
-        puts("");
-        // do checks
         for (size_t i = 0; i < first_line.length; ++i) {
-            char c = first_line.data[i];
-            switch (c) {
-                case 'S':
-                    second_line.data[i] = '|';
-                    break;
-                case '|':
-                    if (second_line.data[i] == '^') {
-                        second_line.data[i-1] = '|';
-                        second_line.data[i+1] = '|';
-                        ++answer1; // beam split
-                    } else {
-                        second_line.data[i] = '|';
-                    }
-                    break;
+            int64_t v = first_line.data[i];
+            if (v == TREE_START) {
+                second_line.data[i] = 1;
+            } else if (v > 0) {
+                if (second_line.data[i] == TREE_SPLIT) {
+                    second_line.data[i-1] = second_line.data[i-1] + v;
+                    second_line.data[i+1] = second_line.data[i+1] + v;
+                    ++answer1; // beam split
+                } else {
+                    second_line.data[i] = second_line.data[i] + v;
+                }
             }
         }
         // swap lines
-        String tmp = first_line;
+        IntArray tmp = first_line;
         first_line = second_line;
         second_line = tmp;
     }
-    // this is final line
-    fwrite(first_line.data, 1, first_line.length, stdout);
-    puts("");
+    // this is final line, calcualte answer 2
+    for(size_t i = 0; i < first_line.length; ++i) {
+        answer2 += first_line.data[i];
+    }
 
-    printf("answer 1: %d\n", answer1);
+    printf("answer 1: %"PRIu64"\n", answer1);
+    printf("answer 2: %"PRIu64"\n", answer2);
 
     free(first_line.data);
     free(second_line.data);
